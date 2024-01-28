e781fb85697a36e00fd9c678b1068d16478af5c7
jGohan-cpu <jjoseriveram01@gmail.com>
2024-01-28 18:25:10 -0400

    3:56

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,32 @@
 "use client"
 
-import { Ghost, MessageSquare, Plus, Trash } from "lucide-react"
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react"
 import UploadButton from "./UploadButton"
 import { trpc } from "@/app/_trpc/client"
 import Skeleton from "react-loading-skeleton"
 import Link from "next/link"
 import { format } from "date-fns"
 import { Button } from "./ui/button"
+import { useState } from "react"
 
 const Dashboard = () => {
 
+    const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<string | null>(null)
+
+    const utils = trpc.useContext()
+
     const { data: files, isLoading } = trpc.getUserFiles.useQuery()
-    const { mutate: deleteFile } = trpc.deleteFile.useMutation()
+    const { mutate: deleteFile, } = trpc.deleteFile.useMutation({
+        onSuccess: () => {
+            utils.getUserFiles.invalidate()
+        },
+        onMutate({ id }) {
+            setCurrentlyDeletingFile(id)
+        },
+        onSettled() {
+            setCurrentlyDeletingFile(null)
+        }
+    })
 
     return (
         <main className='mx-auto max-w-7xl md:p-10'>
@@ -66,7 +81,10 @@ const Dashboard = () => {
                                         size='sm'
                                         className='w-full'
                                         variant='destructive'>
-                                        <Trash className='h-4 w-4' />
+                                        {currentlyDeletingFile === file.id ? (
+                                            <Loader2 className="h-4 w-4 animate-spin" />
+                                        ) : (
+                                            <Trash className='h-4 w-4' />)}
                                     </Button>
                                 </div>
                             </li>
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,41 @@
 import { useState } from "react"
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { Button } from "./ui/button"
+import Dropzone from 'react-dropzone'
+import { Cloud } from "lucide-react"
+
+const UploadDropzone = () => {
+    return <Dropzone multiple={false} onDrop={(acceptedFile) =>
+        console.log(acceptedFile)
+    }>
+        {({ getRootProps, getInputProps, acceptedFiles }) => (
+            <div {...getRootProps()} className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+                <div className='flex items-center justify-center h-full w-full'>
+                    <label
+                        htmlFor='dropzone-file'
+                        className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                    >
+                        <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                            <Cloud className='h-6 w-6 text-zinc-500 mb-2' />
+                            <p className='mb-2 text-sm text-zinc-700'>
+                                <span className='font-semibold'>
+                                    Click to upload
+                                </span>{''}
+                                or drag and drop PDF files here
+                            </p>
+                        </div>
+
+                        {acceptedFiles && acceptedFiles[0] ? (
+                            <div className='max-w-xs bg-white flex items-center rounded:md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
+
+                            </div>
+                        ) : null}
+                    </label>
+                </div>
+            </div>
+        )}
+    </Dropzone>
+}
 
 const UploadButton = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -22,7 +57,7 @@ const UploadButton = () => {
             </DialogTrigger>
 
             <DialogContent>
-                example content
+                <UploadDropzone />
             </DialogContent>
         </Dialog >
     )
